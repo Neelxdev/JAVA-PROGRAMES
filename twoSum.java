e6da219f4bba43f63f889a6490eab3a476c12047
Neelxdev <sayan1962005@gmail.com>
2025-07-14 13:15:27 +0530

    new file created

--- twoSum.java
+++ twoSum.java
@@ -1,16 +1,13 @@
 public class twoSum {
     static int [] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
+            for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+                    return new int[]{i, j}; // Return the indices of the two numbers
                 }
             }
         }
-        return result; // Return empty array if no solution found
+        return null; // Return empty array if no solution found
     }
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
